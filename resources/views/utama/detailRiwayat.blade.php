9030e8953ec4b09d034cec29b918f579f0cfae92
jijeyya <putricempaka2809@gmail.com>
2024-03-27 14:56:49 +0700

    memperbaiki detail riwayat

--- resources/views/utama/detailRiwayat.blade.php
+++ resources/views/utama/detailRiwayat.blade.php
@@ -9,7 +9,7 @@
     <p>tempat lahir : {{ $data->tempat_lahir }}</p>
     <br>
     {{-- jangan lupa kasih format --}}
-    <p>tanggal Lahir : {{ $data->tanggal_lahir }}</p>
+    <p>tanggal Lahir : {{ \Carbon\Carbon::parse($data->tanggal_lahir)->format('d-m-Y') }}</p>
     <br>
     <p>Alamat : {{ $data->alamat }}</p>
     <br>
@@ -19,16 +19,16 @@
     <br>
     <p>pendidikan terakhir : {{ $data->pendidikan_terakhir }}</p>
     <br>
-    <p>waktu pendaftaran : {{ $data->waktu_pendaftaran }}</p>
+    <p>waktu pendaftaran : {{ \Carbon\Carbon::parse($data->waktu_pendaftaran)->format('H:i:s | d-m-Y') }}</p>
     <br>
     <p>kode promo : {{ $data->kode }}</p>
-    <p>potongan : {{ $data->potongan }}</p>
+    <p>potongan : Rp {{ number_format($data->potongan, 0, ',', '.') }}</p>
     <br>
     <p>biaya pendaftaran : Rp 150.000</p>
     <br>
     <p>status pembayaran pendaftaran : {{ $data->status_pembayaran_daftar }}</p>
     <br>
-    <p>biaya diklat : {{ $data->harga_diklat }}</p>
+    <p>biaya diklat : Rp {{ number_format($data->harga_diklat, 0, ',', '.') }}</p>
     <br>
     <p>status pembayaran biaya diklat : {{ $data->status_pembayaran_diklat }}</p>
     <br>
