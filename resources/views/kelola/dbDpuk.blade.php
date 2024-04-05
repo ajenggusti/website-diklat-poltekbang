bddacb93ec3c9819dea2656f4db581be1d299206
cherly sintia <cherly.lilut@gmail.com>
2024-04-05 09:53:38 +0700

    dashboard, navbar, dan kelola dpuk

--- resources/views/kelola/dbDpuk.blade.php
+++ resources/views/kelola/dbDpuk.blade.php
@@ -4,23 +4,21 @@
 <html lang="en">
 <head>
     <title>Document</title>
-    <link href="/css/actor.css" rel="stylesheet">
+    <link href="/css/dashboard.css" rel="stylesheet">
     {{-- Font Poppins --}}
     <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         body {
-            font-family: 'Poppins', sans-serif;
+            /* font-family: 'Poppins', sans-serif; */
         }
-        .content-dashboard {
-            background-color: aqua;
-        }
+    
     </style>
 </head>
 <body>
     <div class="content-dashboard">
-        <p style="color: aqua">Jumlah seluruh pendaftar diklat : {{ $jumlahPendaftar }}</p>
+        <p >Jumlah seluruh pendaftar diklat : {{ $jumlahPendaftar }}</p>
         @foreach($diklatCounts as $diklatCount)
         <div class="card-dashboard">
             <p>Jumlah pendaftar diklat {{ $diklatCount->nama_diklat }}: {{ $diklatCount->total_pendaftar }}</p>
--- resources/views/komponen/dashboardAtas.blade.php
+++ resources/views/komponen/dashboardAtas.blade.php
@@ -12,7 +12,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     {{-- Font Poppins --}}
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fo nts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -62,7 +62,7 @@
             <ul class="nav flex-column">
               {{-- sidebar --}}
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="dbSuperAdmin">
+                <a class="nav-link active" aria-current="page" href="/dbSuperAdmin">
                   <span data-feather="home"></span>
                   Dashboard SuperAdmin
                 </a>
