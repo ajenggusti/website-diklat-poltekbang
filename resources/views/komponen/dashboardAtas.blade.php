ffaf77e02ca7baaf5662f988c2c7661644d4e78d
cherly sintia <cherly.lilut@gmail.com>
2024-04-17 15:34:01 +0700

    Navbar dan Sidebar

--- resources/views/komponen/dashboardAtas.blade.php
+++ resources/views/komponen/dashboardAtas.blade.php
@@ -11,10 +11,10 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    {{-- Font Poppins --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
     <link href="https://fo nts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-    <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
@@ -26,24 +26,12 @@
       body {
         font-family: 'Poppins', sans-serif;
     }
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
     </style>
 
     
-    <!-- Custom styles for this template -->
-    <link href="css.dashboard.css" rel="stylesheet">
+    
+    <link href="/css/dashboard.css" rel="stylesheet">
   </head>
   <body>
     
@@ -64,109 +52,113 @@
     </header>
 
     <div class="container-fluid">
+    {{-- <div class="mainContent"> --}}
       <div class="row">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-          <div class="position-sticky pt-3">
-            <ul class="nav flex-column">
-              {{-- sidebar --}}
-              @can('superAdmin')    
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/dbSuperAdmin">
-                  <span data-feather="home"></span>
-                  Dashboard SuperAdmin
-                </a>
-              </li>
-              @endcan
+        
+        
+          <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light">
+            
+              <div class="position-sticky pt-3">
 
-              @can('dpuk')
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/dbDpuk">
-                  <span data-feather="home"></span>
-                  Dashboard DPUK
-                </a>
-              </li>
-              @endcan
+            
+                  <ul class="nav flex-column">
+                    {{-- sidebar --}}
+                      @can('superAdmin')    
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/dbSuperAdmin">
+                          <span data-feather="home"></span>
+                          Dashboard SuperAdmin
+                        </a>
+                      </li>
+                      @endcan
 
-              @can('keuangan')
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/dbKeuangan">
-                  <span data-feather="home"></span>
-                  Dashboard Keuangan
-                </a>
-              </li>
-              @endcan
-              <li class="nav-item">
-                <a class="nav-link" href="/kelKatDiklat">
-                  <span data-feather="layers"></span>
-                  kelola kategori diklat
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/indexKelolaUser">
-                  <span data-feather="layers"></span>
-                  Kelola user
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/kelPromo">
-                  <span data-feather="layers"></span>
-                  Kelola Promo
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/gbrLandingPage">
-                  <span data-feather="layers"></span>
-                  kelola gmbr Landing Page
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/kelPembayaran">
-                  <span data-feather="layers"></span>
-                  kelola pembayaran
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/kelPendaftaran">
-                  <span data-feather="layers"></span>
-                  kelola pendaftaran
-                </a>
-              </li>
-              
-              <li class="nav-item">
-                <a class="nav-link" href="/kelGambarDiklat">
-                  <span data-feather="layers"></span>
-                  Kelola Gambar diklat
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/kelDiklat">
-                  <span data-feather="layers"></span>
-                  kelola diklat
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/kelTestimoni">
-                  <span data-feather="layers"></span>
-                  kelola testimoni
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        {{-- isi konten ada di dalam main --}}
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            {{-- <h1 class="h2">Dashboard</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-              <div class="btn-group me-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+                      @can('dpuk')
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/dbDpuk">
+                          <span data-feather="home"></span>
+                          Dashboard DPUK
+                        </a>
+                      </li>
+                      @endcan
+
+                      @can('keuangan')
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/dbKeuangan">
+                          <span data-feather="home"></span>
+                          Dashboard Keuangan
+                        </a>
+                      </li>
+                      @endcan
+                      <li class="nav-item">
+                        <a class="nav-link" href="/kelKatDiklat">
+                          <span data-feather="layers"></span>
+                          kelola kategori diklat
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/indexKelolaUser">
+                          <span data-feather="layers"></span>
+                          Kelola user
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/kelPromo">
+                          <span data-feather="layers"></span>
+                          Kelola Promo
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/gbrLandingPage">
+                          <span data-feather="layers"></span>
+                          kelola gmbr Landing Page
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/kelPembayaran">
+                          <span data-feather="layers"></span>
+                          kelola pembayaran
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/kelPendaftaran">
+                          <span data-feather="layers"></span>
+                          kelola pendaftaran
+                        </a>
+                      </li>
+                      
+                      <li class="nav-item">
+                        <a class="nav-link" href="/kelGambarDiklat">
+                          <span data-feather="layers"></span>
+                          Kelola Gambar diklat
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/kelDiklat">
+                          <span data-feather="layers"></span>
+                          kelola diklat
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/kelTestimoni">
+                          <span data-feather="layers"></span>
+                          kelola testimoni
+                        </a>
+                      </li>
+                    {{-- </div> --}}
+                  </ul>
+                {{-- </div> --}}
               </div>
-              <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-              </button>
-            </div> --}}
-          </div>
+            
+          </nav>
+        
+        
+        {{-- isi konten ada di dalam main --}}
+        
+          <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom container-fluid">
+              
+            </div>
 
           
+  
+         
